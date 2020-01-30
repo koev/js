e3b21fd0b17a92195609beb3e144d475cb71f69d
Krasimir Koev <krasimir.koev@techhuddle.com>
2020-01-30 14:07:49 +0200

    1.8

---
+++ react/TheModernReactBootcamp/First_Component/index.js
@@ -0,0 +1,23 @@
+class Hello extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello there!</h1>
+				<h1>Hello there!</h1>
+				<h1>Hello there!</h1>
+			</div>
+		);
+	}
+}
+
+function Hello2() {
+	return (
+		<div>
+			<h1>Hello there2!</h1>
+			<h1>Hello there!</h1>
+			<h1>Hello there!</h1>
+		</div>
+	);
+}
+
+ReactDOM.render(<Hello2 />, document.getElementById('root'));
