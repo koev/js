d61ff5876a21c4881fb8390d53eb19d0b37ca33b
Krasimir Koev <krasi.koev@gmail.com>
2020-02-14 09:38:22 +0200

    import export

---
+++ react/TheModernReactBootcamp/04-create-react-app/my-app/src/helpers.js
@@ -0,0 +1,14 @@
+function helpful() {
+    console.log('this is helpful');
+}
+
+function sort() {
+    console.log('sorted out');
+}
+
+function sing() {
+    console.log('la la la');
+}
+
+export default helpful;
+export { sing , sort};
