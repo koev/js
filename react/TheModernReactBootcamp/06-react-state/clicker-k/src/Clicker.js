1c7a15e4de41e14d8e0532fc0a1aba799fd36d26
Krasimir Koev <krasi.koev@gmail.com>
2020-02-18 14:56:00 +0200

    clicker

---
+++ react/TheModernReactBootcamp/06-react-state/clicker-k/src/Clicker.js
@@ -0,0 +1,27 @@
+import React , {Component} from 'react';
+
+class Clicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            num: 1
+        }
+        this.getRandom = this.getRandom.bind(this);
+    }
+
+    getRandom() {
+        let rand = Math.floor(Math.random() * 10) + 1;
+        this.setState({num:rand})
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Number is {this.state.num}</h1>
+                <button onClick={this.getRandom}>Random Number</button>
+            </div>
+        )
+    }
+}
+
+export default Clicker;
