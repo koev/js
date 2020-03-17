b714455fde3e7641132c3e154d61c07d66721fa6
Krasimir Koev <krasi.koev@gmail.com>
2020-03-17 11:02:42 +0200

    no message

--- react/TheModernReactBootcamp/16-building-yahztee/yahtzee-starter/src/RuleRow.js
+++ react/TheModernReactBootcamp/16-building-yahztee/yahtzee-starter/src/RuleRow.js
@@ -7,12 +7,12 @@ class RuleRow extends Component {
 
   render() {
     const hasScore = this.props.score === undefined;
-    const  { name, score, doScore} = this.props;
+    const  { name, score, doScore, description } = this.props;
 
     return (
       <tr className={`RuleRow RuleRow-${hasScore ? "active" : "disabled"}`} onClick={hasScore ? doScore : null}>
         <td className="RuleRow-name">{name}</td>
-        <td className="RuleRow-score">{score}</td>
+        <td className="RuleRow-score">{hasScore ? description : score}</td>
       </tr>
     )
   }
